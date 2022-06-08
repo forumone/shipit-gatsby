3a69478fbdcc8702de38296327cee1c9f6086b4e
John Gleisner <jgleisner@forumone.com>
2022-06-08 12:03:05 -0500

    header updates

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,17 +7,12 @@ const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
       <span className={styles.logo} />
-      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
+      <span className={styles.navigationItem}>Forum One Team</span>
     </Link>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
         <Link to="/" activeClassName="active">
           Home
-        </Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/" activeClassName="active">
-          Blog
         </Link>
       </li>
     </ul>
